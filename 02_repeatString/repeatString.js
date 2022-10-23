d36134df2f3ed5d91c27b0807396b23b24b0cede
Raheel <raheelbro99@gmail.com>
2022-10-23 19:27:47 -0400

    uncomment .skip() after creating repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,27 @@
-const repeatString = function() {
-
+const repeatString = function(str = "", num = 0) {
+  let finalString = "";
+  if(num >= 0){
+    if(str){
+      for(let i = 0; i < num; i++){
+        finalString += str;
+      }
+      
+      return finalString;
+    }
+    else{
+      return finalString;
+    }
+  }
+  else if(str == ""){
+    return finalString;
+  }
+  else{
+    return "ERROR";
+  }
+  
+  
+  
+  
 };
 
 // Do not edit below this line
