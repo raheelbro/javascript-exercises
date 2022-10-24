424debe48e88e51e015903aaa0564f06822c6307
Raheel <raheelbro99@gmail.com>
2022-10-24 17:38:11 -0400

    define ctof and ftoc functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  //x °F ≘ (x − 32) × 5/9 °C 
+  let f = Math.round(((num-32)*5/9) * 10 ) / 10;
+  return f;
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  // x °C ≘ (x × 9/5 + 32) °F 
+  let c = Math.round((num * 9/5 + 32) * 10) / 10;
+  return c;
 };
 
 // Do not edit below this line
