e8a269cd239e8778418e41bb05c8af0b3229ccff
Raheel <raheelbro99@gmail.com>
2022-10-24 19:27:28 -0400

    add reverseString, removePunctuation and define palindrome functions and remove skip function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,51 @@
-const palindromes = function () {
+const reverseString = function(str) {
+  let finalString = "";
+  let sizeOfString = str.length;
+  for(let i = 0; i <= str.length; i++){
+    finalString += str.substring(sizeOfString-1, sizeOfString);
+    sizeOfString--;
+  }
+  return finalString;
+};
 
+const removePunctuation = function(str){
+  let finalString = "";
+  for (let i = 0; i < str.length; i++){
+    if(str[i] == '.' || str[i] == ',' || str[i] == '?' 
+    || str[i] == '!' || str[i] == ':' || str[i] == ';'
+    || str[i] == '-' || str[i] == '_' || str[i] == '(' 
+    || str[i] == ')' || str[i] == '{' || str[i] == '}'
+    || str[i] == '[' || str[i] == ']' || str[i] == '\''
+    || str[i] == '\"' || str[i] == '`' || str[i] == '~'
+    || str[i] == '#' || str[i] == '$' || str[i] == '%'
+    || str[i] == '^' || str[i] == '&' || str[i] == '*'
+    || str[i] == '+' || str[i] == '=' || str[i] == '@'
+    || str[i] == '<' || str[i] == '>' || str[i] == '/'
+    || str[i] == '\\' || str[i] == ' '){
+      continue;
+    }
+    else{
+      finalString += str[i];
+    }
+  }
+  return finalString;
+}
+
+
+
+const palindromes = function (str) {
+  let noPunct = removePunctuation(str);
+
+  let revString = reverseString(noPunct).toLowerCase();
+  let string = noPunct.toLowerCase();
+
+  if(revString == string){
+    return true;
+  }
+  else{
+    return false;
+  }
+  
 };
 
 // Do not edit below this line
