56f3c2a1115b38af28e34f3e26edbaf4d9b298a1
Raheel <raheelbro99@gmail.com>
2022-10-24 18:01:25 -0400

    define sum func and remove skip func for sum

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,12 @@ const subtract = function(num1, num2) {
   return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for(let i = 0; i < array.length; i++){
+    sum = sum + array[i];
+  }
+  return sum;
 };
 
 const multiply = function() {
