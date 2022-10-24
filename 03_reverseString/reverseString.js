88be07e5c1197ded63d72f0f3fb8283edfbdd9b0
Raheel <raheelbro99@gmail.com>
2022-10-23 20:00:22 -0400

    create reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str = "") {
+  let finalString = "";
+  let sizeOfString = str.length;
+  for(let i = 0; i <= str.length; i++){
+    finalString += str.substring(sizeOfString-1, sizeOfString);
+    sizeOfString--;
+  }
+  return finalString;
 };
 
 // Do not edit below this line
