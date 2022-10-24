f63e9364f35347a45255477f2262b1b41e8755c4
Raheel <raheelbro99@gmail.com>
2022-10-24 15:48:01 -0400

    define sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+  let sum = 0;
+  
+  if(num1 < 0 || num2 < 0 || 
+    typeof(num1) !== typeof(1) || 
+    typeof(num2) !== typeof(1)){
+
+    return "ERROR";
+  }
+  else{
+
+    if(num1 < num2){
+      sum = num1;
+      for (let i = num1; i <= num2; i++){
+        sum = sum + i;
+        
+      }
+    }
+    else{
+      sum = num2;
+      for (let i = num2; i <= num1; i++){
+        sum = sum + i;
+        
+    }
+  }
+  }
+
+return sum -1;
 };
 
 // Do not edit below this line
