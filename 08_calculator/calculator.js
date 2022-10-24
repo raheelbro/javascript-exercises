9fe7fabd1912c4e70cdf7cde42c0555b8113e5d3
Raheel <raheelbro99@gmail.com>
2022-10-24 18:28:58 -0400

    define factorial func and remove skip func for factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -27,8 +27,12 @@ const power = function(number, power) {
   return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let fact = 1;
+	for (let i = number; i > 1; i--){
+    fact = fact * i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
