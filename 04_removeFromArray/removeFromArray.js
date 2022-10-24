918899a31a277416f0601c4103d1877fd2e68f20
Raheel <raheelbro99@gmail.com>
2022-10-24 15:00:14 -0400

    define removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+let array = [];
+const removeFromArray = function(array, ...args) {
 
+
+  for(let i = 0; i < array.length; i++){
+    
+    // if(array[i] == args){
+
+    //   array.splice(i,1);
+
+
+      args.forEach((arg) => {
+        const index = array.indexOf(arg);
+        if (index > -1)
+          array.splice(index, 1);
+        });
+    }
+    
+  console.log(array);
+  return array;
 };
 
 // Do not edit below this line
