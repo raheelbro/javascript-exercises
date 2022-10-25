1aca5e75271d8815574ab430d8d7fbfd83a9d68b
Raheel <raheelbro99@gmail.com>
2022-10-25 11:00:23 -0400

    define getTheTitles func

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (array) {
+  let titles = [];
+  for (let i = 0; i < array.length; i++) {
+    titles[i] = array[i].title;
+    // console.log(titles[i]);
+  }
+  return titles;
 };
 
 // Do not edit below this line
