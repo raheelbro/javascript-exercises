cedac05f9e065e81697a93f30db015861e8cd0cd
Raheel <raheelbro99@gmail.com>
2022-10-25 14:04:15 -0400

    map people array into ages, map ages of people into ids, create loop for checking oldest age and remove skip function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,42 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+
+  const d = new Date();
+  let thisYear = d.getFullYear();
+
+  let ages = people.map(person => ({
+    name: person.name,
+    age: (person.yearOfDeath) ?
+      person.yearOfDeath - person.yearOfBirth :
+      thisYear - person.yearOfBirth
+  }));
+
+  let oldestAge;
+
+
+  //console.log(thisYear + " " + typeof (thisYear));
+  // for (let i = 0; i < ages.length; i++) {
+
+  //   console.log(ages[i].name + " " + ages[i].age);
+
+  // }
+
+  const ids = ages.map(object => {
+    return object.age;
+    // https://bobbyhadz.com/blog/javascript-get-max-id-in-array-of-objects
+  });
+
+  for (let i = 0; i < ages.length; i++) {
+    oldestAge = Math.max(...ids);
+    console.log(oldestAge);
+
+    if (oldestAge == ages[i].age) {
+
+      //console.log(people[i].name + " " + typeof (people[i].name));
+      return ages[i];
+
+    }
+  }
+
 
 };
 
