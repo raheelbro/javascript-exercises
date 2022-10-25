066eb2bcb78174dcc86473bc96efe987f7547470
Raheel <raheelbro99@gmail.com>
2022-10-24 20:23:39 -0400

    define fibonacci function and remove skip function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  let previous = 0;
+  let next = 1;
+  let fib;
+  let num = parseInt(number);
 
+  if (number < 0) {
+    return "OOPS";
+  }
+  else if (number == 1) {
+    return 1;
+  }
+  
+  for (let i = 0; i < num; i++) {
+    if (i == 0) {
+      fib = 0;
+    }
+    else {
+      fib = previous + next;
+      previous = next;
+      next = fib;
+      //console.log(fib);
+    }
+
+  }
+  return fib;
 };
 
 // Do not edit below this line
