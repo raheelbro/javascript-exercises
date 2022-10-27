d3d3d0a89f2f4087a1c97fe7401fca5bc92761d0
Raheel <raheelbro99@gmail.com>
2022-10-27 16:33:15 -0400

    add overflow function to add wrap functionality, need to fix this function somewhat

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,97 +1,165 @@
 const overflow = function (charcode, number) {
 
-  if (number > 0) {
-    let count = charcode;
-    let inc = 1
-    let sum = 0;
+  let count = charcode;
+  let inc = 1
+  let sum = 0;
 
-    for (let i = 1; i <= number; i++) {
-
-      count = count + inc;
-      sum = sum + inc;
-      console.log("sum: " + sum + " count: " + count);
-
-      if (count == 90) {
-        console.log(number - sum);
-        count = 65 + (number - sum);
-        console.log(count);
-        return count;
-      }
-
+  if (number > 25) {
+    while (number > 25) {
+      number = number - 25;
+      console.log(number + "-positive");
     }
-    return count;
+  }
+  else if (number < -25) {
+    while (number < -25) {
+      number = number + 25;
+      console.log(number + "negative");
+    }
   }
   else {
-    let count = charcode;
-    let inc = 1;
-    let sum = 0;
+    number;
+    console.log(number + " 0 or +- 25");
 
-    for (let i = 0; i >= number; i--) {
+  }
 
-      count = count - inc;
-      sum = sum - inc;
-      console.log("sum: " + sum + " count: " + count);
+  if (charcode >= 65 && charcode <= 90) {
+    if (number > 0) {
+      /*
+      adjust overflow if number is greater 
+      than 0 and between 65 and 90
+      */
+      for (let i = 0; i < number; i++) {
 
-      if (count == 65) {
-        console.log(number + sum);
-        count = 90 - (number + sum);
-        console.log(count);
-        return count;
+        count = count + inc;
+        sum = sum + inc;
+        if (count > 90) {
+          count = 65 + (number - sum);
+          return count;
+        }
       }
+      return count;
+    }
+    else if (number < 0) {
+      /*
+      adjust overflow if number is less
+      than 0 and between 65 and 90
+      */
+      // if (number = -1) {
+      //   count = 90;
+      //   return count;
+      // }
+      // else {
+      for (i = number; i < 0;) {
+        count = count - inc;
+
+        sum++;
+
+        console.log("i: " + i + " , sum: " + sum);
+        if (count < 65) {
+          if (i == -1) {
+            count = 90;
+            return count;
+          }
+          else {
+            i++;
+            count = 90 + i;
+            return count;
+          }
+
+        }
+      }
+      // }
+
+
+
+
+      // for (let i = number; i > number; i--) {
+      //   count = count - inc;
+      //   console.log("i: " + i);
+      //   console.log("count: " + count);
+      //   sum = sum + inc;
+      //   console.log("sum: " + sum);
+      //   if (count < 65) {
+      //     count = 90 + (number + i);
+      //     console.log("number + i: " + number + i);
+      //     console.log(count);
+      //     return count;
+      //   }
+      // }
+      return count;
     }
   }
+  else {
+
+    if (number > 0) {
+      /*
+      adjust overflow if number is greater 
+      than 0 and between 97 and 122
+      */
+
+      for (let i = 1; i <= number; i++) {
+
+        count = count + inc;
+        sum = sum + inc;
+        if (count == 122) {
+
+          count = 97 + (number - sum);
+          return count;
+        }
+      }
+      return count;
+    }
+    else {
+      /*
+      adjust overflow if number is less 
+      than 0 and between 97 and 122
+      */
+      for (let i = 0; i >= number; i--) {
+
+        count = count - inc;
+        sum = sum - inc;
+
+        if (count == 97) {
+
+          count = 122 - (number + sum);
+          return count;
+        }
+      }
+      return count;
+    }
+  }
+
 }
-
-
-
-
 
 const caesar = function (string, number) {
 
-  console.log(overflow(67, -5));
+  console.log(overflow(65, -5));
   let charCode = [];
 
+  // checking if the lenght of charCode of the letter
 
-  // for (let i = 0; i < string.length; i++) {
-  //   if ((string.charCodeAt(i) >= 65 &&
-  //     string.charCodeAt(i) <= 90) ||
-  //     (string.charCodeAt(i) >= 97 &&
-  //       string.charCodeAt(i) <= 122)) {
+  for (let i = 0; i < string.length; i++) {
 
-  //     let overflow;
-  //     if (string.charCodeAt(i) + number > 90) {
-  //       for (let j = number; j > 0; j--) {
-  //         overflow = string.charCodeAt(i) + 1;
-  //         if (overflow == 90) {
-  //           overflow = 65 + j;
-  //         }
-  //       }
-  //       charCode[i] = overflow;
-  //     }
-  //     else {
-  //       charCode[i] = string.charCodeAt(i) + number;
-  //     }
+    if ((string.charCodeAt(i) >= 65 &&
+      string.charCodeAt(i) <= 90) ||
+      (string.charCodeAt(i) >= 97 &&
+        string.charCodeAt(i) <= 122)) {
 
-  //   }
-  //   else {
-  //     charCode[i] = string.charCodeAt(i);
-  //   }
+      rightNum = overflow(number);
 
-  // }
-  // console.log(charCode);
+      charCode[i] = string.charCodeAt(i) + rightNum;
+    }
+  }
 
-  // //console.log(charCodeAt(65));
-  // let crypticMsg = "";
+  let crypticMsg = "";
 
-  // for (let i = 0; i < charCode.length; i++) {
+  for (let i = 0; i < charCode.length; i++) {
 
-  //   crypticMsg += String.fromCharCode(parseInt(charCode[i]));
+    crypticMsg += String.fromCharCode(parseInt(charCode[i]));
 
-
-  //   //String.fromCharCode(parseInt(66, 16));
-  // }
-  // console.log(crypticMsg);
-  // return crypticMsg;
+  }
+  console.log(crypticMsg);
+  return crypticMsg;
 };
 
 // Do not edit below this line
